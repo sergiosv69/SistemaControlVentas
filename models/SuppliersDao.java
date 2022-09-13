5ea06d5491c19da45787ad1b436d3c8cbf3891d7
Bremel <djbreme01l@gmail.com>
2022-09-13 09:29:36 -0500

    models SuppliersDao completado(registrar, modificar y eliminar)

--- models/SuppliersDao.java
+++ models/SuppliersDao.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class SuppliersDao {
@@ -33,7 +35,7 @@ public class SuppliersDao {
             pst.setString(5, supplier.getEmail());
             pst.setString(6, supplier.getCity());
             pst.setTimestamp(7, datetime);
-            pst.setTimestamp(7, datetime);
+            pst.setTimestamp(8, datetime);
             pst.execute();  //ejecutamos la consulta
             return true;
         } catch (SQLException e) {
@@ -43,4 +45,77 @@ public class SuppliersDao {
 
     }
 
+    //Listar proveedor
+    public List listSuppliersQuery(String value) {
+        List<Suppliers> list_suppliers = new ArrayList();
+        String query = "SELECT * FROM suppliers";
+        String query_search_supplier = "SELECT * FROM suppliers WHERE name LIKE '%" + value + "%'";
+
+        try {
+            conn = cn.getConnection();
+            if (value.equalsIgnoreCase("")) { //Si no ingresa nada ejecuta esto, sino el else
+                pst = conn.prepareStatement(query);
+                rs = pst.executeQuery();
+            } else {
+                pst = conn.prepareStatement(query_search_supplier);
+                rs = pst.executeQuery();
+            }
+
+            while (rs.next()) {
+                Suppliers supplier = new Suppliers();
+                supplier.setId(rs.getInt("id"));
+                supplier.setName(rs.getString("name"));
+                supplier.setDescription(rs.getString("description"));
+                supplier.setAddress(rs.getString("address"));
+                supplier.setTelephone(rs.getString("telephone"));
+                supplier.setEmail(rs.getString("email"));
+                supplier.setCity(rs.getString("city"));
+                list_suppliers.add(supplier);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return list_suppliers;
+    }
+
+    //Modificar proveedor
+    public boolean updateSupplierQuery(Suppliers supplier) {
+        String query = "UPDATE suppliers SET name = ?, description  = ?, address = ?, telephone = ?, "
+                + "email = ?, city = ?, updated = ? WHERE id = ?";
+
+        Timestamp datetime = new Timestamp(new Date().getTime());
+
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setString(1, supplier.getName());
+            pst.setString(2, supplier.getDescription());
+            pst.setString(3, supplier.getAddress());
+            pst.setString(4, supplier.getTelephone());
+            pst.setString(5, supplier.getEmail());
+            pst.setString(6, supplier.getCity());
+            pst.setTimestamp(7, datetime);
+            pst.setInt(8, supplier.getId());
+            pst.execute();  //ejecutamos la consulta
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al modificar los detos del proveedor");
+            return false;
+        }
+    }
+
+    //Eliminar proveedor
+    public boolean deleteSupplierQuery(int id) {
+        String query = "DELETE FROM suppliers WHERE id = " + id;
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.execute();
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No puedes eliminar un proveedor que tiene relación con otra tabla");
+            return false;
+        }
+    }
 }
