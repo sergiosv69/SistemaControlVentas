a5a3ccdd300feab17f4cfc562b2879f1684044a4
SandroEspinoza <said_3_6@hotmail.com>
2022-09-26 10:35:36 -0500

    CustomersController

--- models/CustomersDao.java
+++ models/CustomersDao.java
@@ -1,4 +1,3 @@
-
 package models;
 
 import java.sql.Connection;
@@ -12,19 +11,20 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class CustomersDao {
-      //Instanciar conexión
+    //Instanciar conexión
+
     ConnectionMySQL cn = new ConnectionMySQL();
     Connection conn;
     PreparedStatement pst;
     ResultSet rs;
-    
-    //registrar cliente
+  
+    //Registrar cliente
     public boolean registerCustomerQuery(Customers customer){
         String query = "INSERT INTO customers (id, full_name, address, telephone, email, created, updated)"
-                + "VALUES (?,?,?,?,?,?,?,?)";
-        
-        Timestamp datetime = new Timestamp (new Date().getTime());
-        try{
+                + "VALUES (?,?,?,?,?,?,?)";
+
+        Timestamp datetime = new Timestamp(new Date().getTime());
+        try {
             conn = cn.getConnection();
             pst = conn.prepareStatement(query);
             pst.setInt(1, customer.getId());
@@ -36,49 +36,48 @@ public class CustomersDao {
             pst.setTimestamp(7, datetime);
             pst.execute();
             return true;
-            
-                    
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "error al registrar cliente");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar cliente");
             return false;
-        
         }
-        
+
     }
-    //listar clientes
-    public List listCustomerQuery(String value){
-    List<Customers> list_customers =new ArrayList(); 
-    String query = " SELECT FROM customers";
-    String query_search_customer = "SELECT FROM customers WHERE id LIKE ´%"+ value +"´%";
-    try{
+
+    //Listar clientes
+    public List listCustomerQuery(String value) {
+        List<Customers> list_customers = new ArrayList();
+        String query = "SELECT * FROM customers";
+        String query_search_customer = "SELECT * FROM customers WHERE id LIKE ´%" + value + "%'";
+         try{
             conn = cn.getConnection();
             if (value.equalsIgnoreCase("")) { //Si no ingresa nada ejecuta esto, sino el else
-            pst = conn.prepareStatement(query);
-            rs = pst.executeQuery();
-        } else {
-            pst = conn.prepareStatement(query_search_customer);
-            rs = pst.executeQuery();
-        }
-        while (rs.next()) {
-            Customers customer = new Customers();
-            customer.setId(rs.getInt("id"));
-            customer.setFull_name(rs.getString("full name"));
-            customer.setAddress(rs.getString("Address"));
-            customer.setTelephone(rs.getString("Telephone"));
-            customer.setEmail(rs.getString("Email"));
-            list_customers.add(customer);
+                pst = conn.prepareStatement(query);
+                rs = pst.executeQuery();
+            } else {
+                pst = conn.prepareStatement(query_search_customer);
+                rs = pst.executeQuery();
             }
-        
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, e.toString());                         
+            while (rs.next()) {
+                Customers customer = new Customers();
+                customer.setId(rs.getInt("id"));
+                customer.setFull_name(rs.getString("full_name"));
+                customer.setAddress(rs.getString("address"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                list_customers.add(customer);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
         }
         return list_customers;
     }
-    //modificar cliente
-    public boolean updateCustomerQuery(Customers customer){
-        String query = "UPDATE customers SET full_name=?, address=?, telephone=?, email=?, updated=?";
-               
-        
+    
+    //Modificar clientes
+    public boolean updateCustomerQuery(Customers customer) {
+        String query = "UPDATE customers SET full_name = ?, address = ?, telephone = ?, email = ?, updated = ? "
+                + "WHERE id = ?";
         Timestamp datetime = new Timestamp (new Date().getTime());
         try{
             conn = cn.getConnection();
@@ -92,16 +91,15 @@ public class CustomersDao {
             pst.setInt(6, customer.getId());
             pst.execute();
             return true;
-            
-                    
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "error al modificar los datos del cliente");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al modificar los datos del cliente");
             return false;
-        
         }
-        
+
     }
-    //eliminar cliente
+    
+    //Eliminar cliente
     public boolean deleteCustomerQuery(int id){
         String query = "DELETE FROM customers WHERE id = " + id;
         try{
@@ -115,4 +113,3 @@ public class CustomersDao {
         }
     }
 }
-
